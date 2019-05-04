5dc2de46f13bc13af04c5075037ce5d57764de24
Junior Quintero <juniorq1995@gmail.com>
2019-05-04 16:55:10 -0700

    test

---
+++ NumCpp/NumCpp.cpp
@@ -0,0 +1,38 @@
+﻿// NumCpp.cpp : Defines the entry point for the application.
+//
+
+#include "NumCpp.h"
+
+using namespace std;
+
+NumCpp::~NumCpp()
+{
+
+}
+
+NumCpp::NumCpp()
+{
+
+}
+
+NumCpp::NumCpp(int num, ...)
+{
+	/* Initializing arguments to store all values after num */
+	va_start(arguments, num);
+	/* Sum all the inputs; we still rely on the function caller to tell us how
+	 * many there are */
+	for (int i = 0; i < num; i++)
+	{
+		intList.push_back(va_arg(arguments, int));
+	}
+	va_end(arguments);                  // Cleans up the list
+
+}
+
+void NumCpp::Print()
+{
+	for (int i = 0; i < intList.size(); i++)
+	{
+		cout << intList.at(i);
+	}
+}
---
+++ NumCpp/NumCpp.h
@@ -0,0 +1,25 @@
+﻿// NumCpp.h : Include file for standard system include files,
+// or project specific include files.
+// https://webcourses.ucf.edu/courses/1249560/pages/cmake-setting-up-a-library-c++-code-overview?module_item_id=10524230
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <stdarg.h>
+
+// TODO: Reference additional headers your program requires here.
+class NumCpp
+{
+private:
+	va_list arguments;
+	vector<int> intList;
+	enum Type {IntList, DoubleList, CharList};
+	Type type;
+
+public:
+	~NumCpp();
+	NumCpp();
+	NumCpp(int num, ...);
+	void Print();
+};
