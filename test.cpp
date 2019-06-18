946f9e3e66cab8e6889533c4e70ea0e0310a026a
Junior Quintero <juniorq1995@gmail.com>
2019-06-17 22:12:30 -0700

    Added catch test

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include "NumCpp.hpp"
+#include "catch.hpp"
+
+TEST_CASE("Vectors", "Create vector") {
+	NumCpp test = NumCpp({ 1,2 });
+	REQUIRE(test.get(0) == 1);
+	REQUIRE(test.get(1) == 2);
+
+}
